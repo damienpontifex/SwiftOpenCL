8c7eb1428a43f7a0cbc3506ad49d8a253f7aa77f
Damien Pontifex <damien.pontifex@gmail.com>
2015-12-28 19:14:17 +0800

    added xcode project for dev

---
+++ SwiftOpenCL/SwiftOpenCL.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOpenCL.h
+//  SwiftOpenCL
+//
+//  Created by Damien Pontifex on 28/12/2015.
+//  Copyright © 2015 Pontifex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftOpenCL.
+FOUNDATION_EXPORT double SwiftOpenCLVersionNumber;
+
+//! Project version string for SwiftOpenCL.
+FOUNDATION_EXPORT const unsigned char SwiftOpenCLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOpenCL/PublicHeader.h>
+
+
